6877eeacb91d2920e349cb8e4863bd47ab1e0440
chasingdonis <cuixianning@hotmail.com>
2015-09-19 09:27:01 -0500

    jv editing

---
+++ script.js
@@ -0,0 +1,40 @@
+var article = {
+'name': "Winston Churchill",
+'year': 1940,
+'yearBce': false
+};
+
+var article2 = {
+'name':"Ghamdi",
+'year': 1942,
+'yearBce': false
+};
+
+var article3 = {
+'name': "Demosthenes",
+'year': 342,
+'yearBce': true
+};
+
+var speech = ['article', 'article2', 'article3'];
+
+var yearDistance = article2.year-article.year;
+
+console.log(artical2.name + "and" + article.name + "'s speech are" + yearDistance + "apart.");
+
+
+document.getElementById('BtnDonate').addEventListener('click', function(){
+  console.log("There are" + speech.length + "speeches on the page.")
+});
+
+document.getElementById('BtnChurchill').addEventListener('cl90ick', function(){
+    console.log("This speech was written by" + article.name + "in" + article.year + "it is" + article.yearBce + "that this year is B.C.E")
+});
+
+document.getElementById('BtnGhandi').addEventListener('click', function(){
+   console.log("This speech was written by" + article2.name + "in" + article2.year + "it is" + article2.yearBce + "that this year is B.C.E")
+});
+
+document.getElementById('BtnDemosthenes').addEventListener('click', function(){
+   console.log("This speech was written by" + article3.name + "in" + article3.year + "it is" + article3.yearBce + "that this year is B.C.E")
+});
