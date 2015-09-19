a59d02a1521b4cc09b19bf11ceb00270ae8b2d09
chasingdonis <cuixianning@hotmail.com>
2015-09-19 09:39:05 -0500

    fixing js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ var article = {
 };
 
 var article2 = {
-'name':"Ghamdi",
+'name': "Ghamdi",
 'year': 1942,
 'yearBce': false
 };
@@ -20,21 +20,21 @@ var speech = ['article', 'article2', 'article3'];
 
 var yearDistance = article2.year-article.year;
 
-console.log(artical2.name + "and" + article.name + "'s speech are" + yearDistance + "apart.");
+console.log(article2.name + "and" + article.name + "'s speech are" + yearDistance + "years apart.");
 
 
 document.getElementById('BtnDonate').addEventListener('click', function(){
-  console.log("There are" + speech.length + "speeches on the page.")
+  console.log("There are" + speech.length + "speeches on the page.");
 });
 
-document.getElementById('BtnChurchill').addEventListener('cl90ick', function(){
-    console.log("This speech was written by" + article.name + "in" + article.year + "it is" + article.yearBce + "that this year is B.C.E")
+document.getElementById('BtnChurchill').addEventListener('click', function(){
+    console.log("This speech was written by" + article.name + "in" + article.year + "it is" + article.yearBce + "that this year is B.C.E");
 });
 
 document.getElementById('BtnGhandi').addEventListener('click', function(){
-   console.log("This speech was written by" + article2.name + "in" + article2.year + "it is" + article2.yearBce + "that this year is B.C.E")
+   console.log("This speech was written by" + article2.name + "in" + article2.year + "it is" + article2.yearBce + "that this year is B.C.E");
 });
 
 document.getElementById('BtnDemosthenes').addEventListener('click', function(){
-   console.log("This speech was written by" + article3.name + "in" + article3.year + "it is" + article3.yearBce + "that this year is B.C.E")
+   console.log("This speech was written by" + article3.name + "in" + article3.year + "it is" + article3.yearBce + "that this year is B.C.E");
 });
